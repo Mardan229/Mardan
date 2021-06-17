44a0d6232c6a3d737633bc2915eeba2723b16118
shahin <shahinurakter085bd@gmail.com>
2021-06-16 23:23:26 -0400

    commit

--- Mardan229/src/test/java/Mardar229/mardan229.java
+++ Mardan229/src/test/java/Mardar229/mardan229.java
@@ -4,6 +4,15 @@ public class mardan229 {
 
 	public static void main(String[] args) {
 System.out.print("Hi Group");
+	System.out.println("Now I am so happy because I cloned the project ");
+	
+	
+	
+	
 	}
+	
+	
+	
+	
 
 }
